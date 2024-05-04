db9ebe45550caed5fadd3737ce66462e15f89dc7
ajuaji0504 <ajithveera0504@gmail.com>
2024-05-04 11:53:30 +0530

    resume

---
+++ task3.js
@@ -0,0 +1,25 @@
+{
+    name: "Ajith",
+    "contact"; {
+      "email"; "ajithveera0504@gmail.com",
+      "phone"; "123-456-7890",
+      "address"; "123 Main St, City, State, Zip"
+    }
+}
+    {
+        "degree"; "Bachelor of Science in Computer Science",
+        "university"; "SRM University ",
+        "location"; "chennai, tamil nadu",
+        "graduation_year"; "2018 "
+      }
+    
+    "skills";{
+      "Java",
+      "Python",
+      "JavaScript",
+      "SQL",
+      "Agile Methodologies",
+      "Problem Solving",
+      "Teamwork"
+    }
+    
